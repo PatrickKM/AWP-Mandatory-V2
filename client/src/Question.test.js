18f7fae2e84a88386808d5a96582ba8498fa53e9
PatrickKM <32448400+PatrickKM@users.noreply.github.com>
2020-04-19 20:00:15 +0200

    Update Question.test.js

--- client/src/Question.test.js
+++ client/src/Question.test.js
@@ -6,11 +6,27 @@ import Question from "./Question";
 const question = {
     _id: 23,
     question: "How are you doing",
-    answers: []
+    answers: [{_id: 55, answerText: "Fandango", votes: 1},{_id: 56, answerText: "Fandango2", votes: 3},{_id: 55, answerText: "Fandango5", votes: 7}]
 };
-
 it('renders the question',()=>{
     const comp = <Question getQuestion={_id => question}/>
     const {getByText, getByLabelText} = render(comp);
     expect(getByText(question.question)).toBeInTheDocument();
+});
+
+it(' it renders all the answers',()=>{
+    const comp = <Question getQuestion={_id => question}/>
+    const {getByText, getByLabelText} = render(comp);
+    expect(getByText(question.answers[0].answerText)).toBeInTheDocument();
+    expect(getByText(question.answers[1].answerText)).toBeInTheDocument();
+    expect(getByText(question.answers[2].answerText)).toBeInTheDocument();
+});
+
+it('calls "onSubmit" when the voting button is clicked', () => {
+    const onSubmit = jest.fn();
+    const comp = <Question getQuestion={_id => question}
+                           putVote={onSubmit}/>
+    const {getAllByText} = render(comp);
+    fireEvent.click(getAllByText(/Likes/i)[0]); /* <!-- props.putVote is not a function*/
+    expect(onSubmit).toHaveBeenCalled();
 });
